0d4e049de7ffb4176cb3d6087a4ad72219280b1d
praneethS435 <saipraneeth.sristi@gmail.com>
2025-02-03 11:42:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Grid from "./components/Grid";
+import { Footer } from "./components/Footer";
+function App() {
+  
+  return (
+    <>
+      <Header></Header>
+      <Grid></Grid>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
