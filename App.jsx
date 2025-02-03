82c84f09bb32dc3b366cc0794a69023b7087e0d3
praneethS435 <saipraneeth.sristi@gmail.com>
2025-02-03 12:04:24 +0530

    updated header and footer comps

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+import Grid from "./components/Grid";
+import { Footer } from "./components/Footer";
+function App() {
+  
+  return (
+    <>
+      <Header></Header>
+      <Grid></Grid>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
